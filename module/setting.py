520eb68e9890ce6b7cdef30df97a09215f574173
Katon Dorojatun <katonroja@gmail.com>
2022-03-12 23:42:14 +0900

    creating setting file

---
+++ module/setting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 23:37:54 2022
+
+@author: Katon Dorojatun
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.optimize as opt
+import copy
+from dna_features_viewer import GraphicFeature, GraphicRecord, CircularGraphicRecord
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.sans-serif"] = "Times New Roman"
+plt.rcParams['font.size'] = "9"
