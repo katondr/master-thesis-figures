be2d0ac358a83d8812e7f0a28967baa0eef00fa6
Katon Dorojatun <katonroja@gmail.com>
2022-03-12 23:53:39 +0900

    changing import scheme

--- module/thesis-34y-luxri-map.py
+++ module/thesis-34y-luxri-map.py
@@ -3,17 +3,9 @@
 """
 Created on Thu Jul 29 12:51:08 2021
 
-@author: tony
+@author: Katon Dorojatun
 """
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scipy.optimize as opt
-import copy
-from dna_features_viewer import GraphicFeature, GraphicRecord, CircularGraphicRecord
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.sans-serif"] = "Times New Roman"
-plt.rcParams['font.size'] = "9"
+from setting import *
 #%%
 counter = 0
 features=[
@@ -57,8 +49,8 @@ record = GraphicRecord(sequence_length=3030, features=linear_features)
 fig, _ = record.plot(figure_width=6, figure_height=1.5)
 fig.set_xticks(np.arange(0, 3200, 750))
 
-fig.figure.tight_layout()
+#fig.figure.tight_layout()
 
 #fig.savefig("../figure/thesis-34y-luxri.png", dpi=300, transparent=True)
 #fig.figure.savefig("../figure/thesis-34y-luxri-map.pdf")
-#fig.figure.savefig("../figure/thesis-34y-luxri-map.png", dpi=300, bbox_inches='tight', transparent=True)
+fig.figure.savefig("../figure/thesis-34y-luxri-map.png", dpi=300, bbox_inches='tight', transparent=True)
