3ffebed39a3448d02d3e02696db2be7b957fe04e
Katon Dorojatun <katonroja@gmail.com>
2022-03-14 00:02:58 +0900

    generating graph

--- module/thesis-34y-luxri-graph.py
+++ module/thesis-34y-luxri-graph.py
@@ -3,17 +3,11 @@
 """
 Created on Thu Jul 29 12:51:08 2021
 
-@author: tony
+@author: Katon Dorojatun
 """
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scipy.optimize as opt
-import copy
-from dna_features_viewer import GraphicFeature, GraphicRecord, CircularGraphicRecord
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.sans-serif"] = "Times New Roman"
-plt.rcParams['font.size'] = "9"
+
+from setting import *
+
 #%%
 data = pd.read_csv('../data/fl-210615.csv')
 #time = data.t.unique().tolist()[:-1]
